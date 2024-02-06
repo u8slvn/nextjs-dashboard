04eb686bffb5b0f1ea427da80dda1f740c53b976
Sylvain Collas <sylvain.collas@ubisoft.com>
2024-02-06 12:01:54 +0100

    Chapter 6

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
@@ -22,7 +21,6 @@ export default function Page() {
             </a>
             , brought to you by Vercel.
           </p>
-          <div className={styles.shape} />
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
